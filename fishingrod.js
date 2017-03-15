756f81aa4a1b7a9dfc6cfdf57215a92f4dead574
Jo Colina <jo@jocolina.com>
2017-03-15 16:47:18 +0100

    Added support for strings

--- fishingrod.js
+++ fishingrod.js
@@ -2,6 +2,23 @@ var request = require('./lib/request.js');
 var fishingrod = {};
 
 fishingrod.fish = function(params, callback){
+	if(typeof params === 'string'){
+		const URL = require('url');
+		const QS = require('querystring');
+		var params = URL.parse(params);
+		if(params.query){
+			params.data = QS.parse(params.query); 
+			params.path = params.pathname;
+		}
+		if(params.protocol){
+			if(params.protocol === 'http:'){
+				params.https = false;
+			} else if (params.protocol === 'https:'){
+				params.https = true;
+			}
+		}
+	}
+
 	if(!params.method) { params.method = 'GET'; }
 	if(!params.https) { params.https = false; }
 
